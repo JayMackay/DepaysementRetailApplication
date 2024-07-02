7f4d1f8f2ee6174b1146636b5f452137e465d237
JaredMackay <JaredMackay@paypoint.com>
2024-07-02 12:48:38 +0100

    Implement base CosmosDB logic with initial CRUD operation stubs

---
+++ DepaysementRetailApplication.Infrastructure/CosmosUpdaters/BaseCosmosUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DepaysementRetailApplication.Infrastructure.Interfaces;
+
+namespace DepaysementRetailApplication.Infrastructure.CosmosUpdaters
+{
+    public class BaseCosmosUpdater<T> : IBaseCosmosUpdater<T>
+    {
+        public async Task<T> GetCurrentItemOrNull(DbContext context, string id, string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetMergedItem(T previousData, T existingData, T newData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<HttpStatusCode> TryUpsertItemAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
